af3fb83da5774cd1bab06806eb5a8b285cf14f9d
pradeep <thomaspradeep06@gmail.com>
2024-12-19 21:24:53 +0530

    new A record

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
 resource "aws_route53_zone" "primary" {
   name = "testdomain.com"
 }
+
+resource "aws_route53_record" "dev_record" {
+    zone_id = aws_route53_zone.primary.zone_id
+    name = "dev"
+    type = "A"
+
+    alias {
+        name = testdomain.com
+        zone_id = aws_route53_zone.primary.zone_id
+        evaluate_target_health = true
+    }
+}
