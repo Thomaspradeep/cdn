4d2181a413019e41e8521058b4a0a3ec4e34f25e
pradeep <thomaspradeep06@gmail.com>
2024-12-20 10:01:19 +0530

    testing

--- main.tf
+++ main.tf
@@ -2,14 +2,10 @@ resource "aws_route53_zone" "primary" {
   name = "testdomain.com"
 }
 
-resource "aws_route53_record" "dev_record" {
-    zone_id = aws_route53_zone.primary.zone_id
-    name = "dev"
-    type = "A"
-
-    alias {
-        name = "54.92.210.189"
-        zone_id = aws_route53_zone.primary.zone_id
-        evaluate_target_health = true
-    }
+resource "aws_route53_record" "ns" {
+    count = var.parent_zone_ids.internal !=null ? 1:0
+    zone_id = var.parent_zone_ids.internal
+    name = "testdomain.com"
+    type = "NS"
+    records = aws_route53_zone.primary.name_servers
 }
